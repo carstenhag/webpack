a427aebf52c0f0d229d858373cb0bfad7de062c4
Carsten Hagemann <carsten@chagemann.de>
2018-04-26 21:52:11 +0200

    Fix missing things from template

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<template lang="pug">
   #app
     router-view
 </template>
---
+++ template/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .home
+    p Home
+</template>
+
+<script>
+export default {
+  name: 'home',
+  metaInfo: {
+    title: 'Home'
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '../stylus/colors.styl'
+
+</style>
